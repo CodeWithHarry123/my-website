1d60161b6202bc9c89409bcccffea4d63a7c5769
HARRY <rabtaali2@gmail.com>
2025-05-02 19:29:21 +0530

    Update script.js
    123

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ function gameLoop(timestamp) {
     birdTop = 0;
     velocity = 0;
   }
-  if (birdTop > 470) {
+  if (birdTop > 490) {
     console.log("Game Over: Bird hit ground, birdTop=", birdTop);
     endGame();
     return;
